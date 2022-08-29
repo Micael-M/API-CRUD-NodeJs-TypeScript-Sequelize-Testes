1823c39bef9eacc6833c445897370fb91d3c9889
Micael Santos <micaelmaicon@hotmail.com>
2022-08-29 19:03:34 -0300

    feat: add checkPassword

--- app/backend/src/service/loginService.ts
+++ app/backend/src/service/loginService.ts
@@ -1,13 +1,20 @@
+import { compare } from 'bcryptjs';
 import { ILogin } from '../interfaces/interface';
 import User from '../database/models/User';
+
+const checkPassword = async (password: string, payload: string) => {
+  const resultCheck = await compare(password, payload);
+  return resultCheck;
+};
 
 const login = async (credentials: ILogin) => {
   const { email, password } = credentials;
   const resultLogin = await User.findOne({ where: { email } });
-
   if (resultLogin === null) {
     return undefined;
   }
+  const verifyPassword = await checkPassword(password, resultLogin.password);
+  if (!verifyPassword) return { error: { message: 'Incorrect email or password' } };
 
   return resultLogin;
 };
