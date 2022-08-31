6c7b6511d0a3fbcc599d516f7579b753911841af
Micael Santos <micaelmaicon@hotmail.com>
2022-08-31 13:01:01 -0300

    feat: add email validation

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -1,12 +1,17 @@
 import { Router, Request, Response } from 'express';
 import loginService from '../service/loginService';
+import loginValidate from './middlewares/loginValidate';
 
 const loginController = Router();
 
-loginController.post('/', async (req: Request, res: Response) => {
-  const credentials = req.body;
-  const resultLogin = await loginService.login(credentials);
-  return res.status(200).json(resultLogin);
-});
+loginController.post(
+  '/',
+  loginValidate.validateLogin,
+  async (req: Request, res: Response) => {
+    const credentials = req.body;
+    const resultLogin = await loginService.login(credentials);
+    return res.status(200).json(resultLogin);
+  },
+);
 
 export default loginController;
---
+++ app/backend/src/controllers/middlewares/loginValidate.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+const validateLogin = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: 'All fields must be filled' });
+  }
+  next();
+};
+
+export default {
+  validateLogin,
+};
